8ad3014778790c0236834c1aa02636bacbb83493
Bjarne Johannsen <bjarne.johannsen@protonmail.com>
2021-04-02 22:41:05 +0100

    Added Readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # nimbus-c
+
+## Adding a custom processing stage
+### C-Interface
+After having built and installed nimbus-userland (make install), all libraries are installed under /usr/local/lib, all headers are installed under /usr/local/include The header nimbusPreprocessInterface.h contains a low level c interface to libnimbus-preprocessing.so. This is useful when you want to integrate the nimbus 3d with external applications.
+
+The following example (src/main.cpp) shows a basic implemenation to get the image data in C/C++;
+It is important to understand, that this implementation directly integrates into the preprocessing toolchain. Therefore, it is triggered synchronous and any runtime inside the callback will reduce the overall framerate!
+
+## Run the example
+
+Create a build folder
+
+``` 
+mkdir build
+cd build
+``` 
+Run Cmake
+``` 
+cmake ..
+make all
+``` 
+Run the compiled program
+``` 
+./nimbus-c
+``` 
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,25 @@
 int g_cnt;
 
 void imageCallback(void* unused0, void* img, void* unused1) {
-    uint16_t* ampl = nimbus_seq_get_amplitude(img);
-    uint8_t* conf = nimbus_seq_get_confidence(img);
+    //Get Image Data
+    //Image Data contained in a 1D array. 352*286 pixels. 
+    uint16_t* ampl      = nimbus_seq_get_amplitude(img);
+    uint8_t* conf       = nimbus_seq_get_confidence(img);
+    uint16_t* radial    = nimbus_seq_get_radial_dist(img);
+    int16_t* x          = nimbus_seq_get_x(img);
+    int16_t* y          = nimbus_seq_get_y(img);
+    int16_t* z          = nimbus_seq_get_z(img);
     ImgHeader_t* header = nimbus_seq_get_header(img);
     printf("got an image\n");
-    g_cnt++;
+
+    //Change Exposure Settings
+    AutoExposureParams_t m_params;
+    m_params.amplitude     = 1000;    //0.0 - ~1500.0
+    m_params.max_exposure  = 32767;   //0-32767
+    m_params.hdr_factor    = 0.3;     //0.0 - 1.0
+    m_params.exposure_mode = 3;       //MANUAL = 0, MANUAL_HDR  = 1, AUTO = 2, AUTO_HDR = 3
+    bool set_exposure      = nimbus_autoexposure_set_params(&m_params);
+    
     nimbus_seq_del(img); //<- free the image pointer this call is necessary to return img resource to nimbus
 }
 
